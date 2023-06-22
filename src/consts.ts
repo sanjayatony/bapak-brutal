0f3c8e2eeec3b904101e044e5802eaadb55a1ed5
Tony S <sanjayatony@gmail.com>
2023-06-22 11:29:28 +0800

    adding some stuff

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Bapak Dev.";
-export const SITE_DESCRIPTION = "Seorang bapak dari 2 orang anak sekaligus seorang web developer";
+export const SITE_DESCRIPTION =
+	"I am a father with over 10 years of experience as a web developer, specializing in WordPress, Next.js, Astro, and Tailwind CSS. Since 2015, I have been successfully working remotely, embracing the flexibility and freedom it offers.";
